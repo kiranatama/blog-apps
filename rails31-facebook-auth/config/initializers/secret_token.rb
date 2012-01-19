3e22178384f62acad0f62505889d5549ad716bce
William Notowidagdo <wnotowidagdo@gmail.com>
2012-01-20 02:36:36 +0700

    Initial Rails app

---
+++ rails31-facebook-auth/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails31FacebookAuth::Application.config.secret_token = '64853a3dfcc5aeed2b60d1ef32e47ed1abfe77b7e57f98d4502e3917a9551f2240693c9ce37007acf29e0a3759a71a6fc92477481415d9efe66ada278ca89681'
