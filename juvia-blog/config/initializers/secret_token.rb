13651c7fdd930508d88ffc72e45eb6f48c4539c7
williamn <wnotowidagdo@gmail.com>
2012-01-28 14:51:36 +0700

    Empty Rails app

---
+++ juvia-blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JuviaBlog::Application.config.secret_token = 'e4f3e34447ec25f5f19d6dd0141a5ccf6ab21ae197f856760d81c40f53fdfc6aacb4267b23b51a56eef9b352f1cfd4ae51fae2ad73835ab53b8d59d586ca99b9'
