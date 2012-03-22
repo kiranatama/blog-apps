4dd9a7401353208b4fc6cfe88d0603fd8a27d2ca
willamn <wnotowidagdo@gmail.com>
2012-03-22 14:39:18 +0700

    Initial commit

---
+++ using-delicious/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UsingDelicious::Application.config.secret_token = '4d6cda1801ae8b6fbdf885be5ede0193a9a4e3150e8ba1e23e99813f288a713f36a71edf3f1a206514b8fb2f5eafd5406815ade31b361b64320784a66ccb6afd'
