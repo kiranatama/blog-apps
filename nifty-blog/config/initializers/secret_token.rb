68dfcd352232708092f8c2509b0f0ebdf053964a
willamn <wnotowidagdo@gmail.com>
2012-04-20 09:12:09 +0700

    Initial commit for a new app: nifty-blog

---
+++ nifty-blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NiftyBlog::Application.config.secret_token = 'ddbe28e90c96c545ae862182fb1fa18ce192946ded44f6821b765f9603b3099310f664b29d7162a454f19bcbf857632520f7bde9334887de580ce69153d68d28'
