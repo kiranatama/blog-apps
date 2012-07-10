315311888d2455f4f789ace7582dc50b3556c00e
williamn <wnotowidagdo@gmail.com>
2012-07-10 15:41:24 +0700

    + Post belongs to Category

--- aa-blog/app/models/post.rb
+++ aa-blog/app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
-  attr_accessible :body, :title
+  attr_accessible :body, :title, :category_id
   validates :title, :body, :presence => true
+  belongs_to :category
 end
