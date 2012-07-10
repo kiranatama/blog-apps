a73f79e9eb3729fb123016065b30e6c45deb69f4
williamn <wnotowidagdo@gmail.com>
2012-07-10 12:00:17 +0700

    + New app aa-blog. Initial commit

---
+++ aa-blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AaBlog::Application.config.secret_token = 'd4560426bae99ca68184547bcd8d41cec6eed32b0447401ba89bb4763678732a4d49f55a7df07b9e0eb2ed8a9772157a301b71223369940c2594920734353b6a'
